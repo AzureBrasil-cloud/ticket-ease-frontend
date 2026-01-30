a1c9293630db347da755daecb29d83713415d52c
Felipe Faria <afariaflp@gmail.com>
2026-01-30 09:54:25 -0300

    Add logotipo

--- app/components/TeamsMenu.vue
+++ app/components/TeamsMenu.vue
@@ -43,15 +43,6 @@ const items = computed<DropdownMenuItem[][]>(() => {
 </script>
 
 <template>
-  <UDropdownMenu :items="items" :content="{ align: 'center', collisionPadding: 12 }"
-    :ui="{ content: collapsed ? 'w-40' : 'w-(--reka-dropdown-menu-trigger-width)' }">
-    <UButton v-bind="{
-      ...selectedTeam,
-      label: collapsed ? undefined : selectedTeam?.label,
-      trailingIcon: collapsed ? undefined : 'i-lucide-chevrons-up-down'
-    }" color="neutral" variant="ghost" block :square="collapsed" class="data-[state=open]:bg-elevated"
-      :class="[!collapsed && 'py-2']" :ui="{
-        trailingIcon: 'text-dimmed'
-      }" />
-  </UDropdownMenu>
+  <img v-if="collapsed" src="/img/symbol-ticketease.svg" alt="Company Symbol" class="h-8 w-auto" />
+  <img v-else src="/img/logo-ticketease.svg" alt="Company Logo" class="h-8 w-auto" />
 </template>
